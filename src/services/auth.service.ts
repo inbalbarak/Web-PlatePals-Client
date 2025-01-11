b0db705cb9da7d21c0f5cfeff38d6f426dcadbaf
inbal <inbalbarak123@gmail.com>
2025-01-11 10:11:36 +0200

    remove refresh from login

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,6 +1,5 @@
 import { UserAttributes } from "src/interfaces/user.interface";
 import apiClient from "./axiosInstance";
-import { REFRESH_TOKEN } from "constants/localStorage";
 
 type tTokens = {
   accessToken: string;
@@ -10,29 +9,17 @@ type tTokens = {
 const prefix = "auth";
 
 export const googleLogin = async (credential: string) => {
-  try {
-    const res = await apiClient.post<tTokens>(`${prefix}/google-login`, {
-      credential,
-    });
+  const res = await apiClient.post<tTokens>(`${prefix}/google-login`, {
+    credential,
+  });
 
-    return res.data;
-  } catch (_err) {
-    const refreshToken = localStorage.getItem(REFRESH_TOKEN);
-
-    return await refresh(refreshToken ?? "");
-  }
+  return res.data;
 };
 
 export const login = async (user: UserAttributes) => {
-  try {
-    const res = await apiClient.post<tTokens>(`${prefix}/login`, user);
+  const res = await apiClient.post<tTokens>(`${prefix}/login`, user);
 
-    return res.data;
-  } catch (_err) {
-    const refreshToken = localStorage.getItem(REFRESH_TOKEN);
-
-    return await refresh(refreshToken ?? "");
-  }
+  return res.data;
 };
 
 export const register = async (user: UserAttributes) => {
