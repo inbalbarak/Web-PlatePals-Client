4f3adb7041d9c33d054d29e95a6fa610e3484815
yahligitzi <yahligitzi2@gmail.com>
2025-03-16 16:34:42 +0200

    fix vite env vars

--- src/services/axiosInstance.ts
+++ src/services/axiosInstance.ts
@@ -13,13 +13,19 @@ export const isTokenValid = () => {
   if (isNaN(date.getTime())) {
     throw new Error("Invalid date string");
   }
+
+  
+  console.log("clientid", import.meta.env.VITE_CLIENT_ID)
+  console.log("backendUrl", import.meta.env.VITE_BACKEND_URL)
+  console.log("extraHours", import.meta.env.VITE_TOKEN_EXPIRES_HOURS)
   date.setHours(
-    date.getHours() + parseInt(import.meta.env.VITE_TOKEN_EXPIRES_HOURS ?? "")
+    date.getHours() + parseInt(import.meta.env.VITE_TOKEN_EXPIRES_HOURS ?? "3")
   );
 
   return date > new Date();
 };
 
+console.log("again backendurl", import.meta.env.VITE_BACKEND_URL)
 const apiClient = axios.create({ baseURL: import.meta.env.VITE_BACKEND_URL });
 
 apiClient.interceptors.request.use(async (request) => {
@@ -28,13 +34,16 @@ apiClient.interceptors.request.use(async (request) => {
     let authToken = "";
     if (isValid) {
       authToken = localStorage.getItem(ACCESS_TOKEN) ?? "";
+      console.log("token is valid", authToken)
     } else {
+      console.log("token isn't valid - refresh", REFRESH_TOKEN)
       const tokens = await refresh(localStorage.getItem(REFRESH_TOKEN) ?? "");
 
       localStorage.setItem(ACCESS_TOKEN, tokens.accessToken);
       localStorage.setItem(REFRESH_TOKEN, tokens.refreshToken);
       localStorage.setItem(TOKEN_TIMESTAMP, new Date().toString());
 
+      console.log("refresh and set current", tokens.accessToken)
       authToken = tokens.accessToken;
     }
 
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,10 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import importMetaEnv from "@import-meta-env/unplugin";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
         react(),
-        importMetaEnv.vite({
-            example: ".env.example.public",
-            transformMode: process.env.NODE_ENV === "development" ? "compile-time" : "runtime",
-        }),
     ],
     resolve: {
         // aliases should be added both here and in tsconfig.json
